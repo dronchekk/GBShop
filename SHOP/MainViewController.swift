93078ce29c475d5e61a0dbf87f2f05432d3f8cc4
Andrey rachitsky <>
2022-05-10 19:42:29 +0300

    Less3 Testing

--- SHOP/MainViewController.swift
+++ SHOP/MainViewController.swift
@@ -15,7 +15,7 @@ class MainViewController: UIViewController {
     init() {
         super.init(nibName: nil, bundle: nil)
     }
-
+    
     @available(*, unavailable)
     required init?(coder _: NSCoder) {
         fatalError("init(coder:) has not been implemented")
